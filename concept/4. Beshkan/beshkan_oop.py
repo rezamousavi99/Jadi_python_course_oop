61dc84275b3e1c035d8a1c183f1fa36c5c8d7603
rezamousavi <rezamoosavi461@gmail.com>
2023-07-21 17:51:02 +0330

    add beshkan_oop.py

---
+++ concept/4. Beshkan/beshkan_oop.py
@@ -0,0 +1,37 @@
+from random import randrange
+
+class Beshkan:
+    def __init__(self, people_number=50) -> None:
+        self.people = []
+        self.people_number = people_number
+        self.person1 = 0
+        self.person2 = 0
+
+        # dedicate 100$ for each person
+        for i in range(0, self.people_number):
+            self.people.append(100)
+
+    def beshkan(self, beshkan_number=200):
+        for _ in range(0, beshkan_number):
+            for self.person1 in range(0, self.people_number):
+                self.person2 = randrange(0, self.people_number)
+
+                while self.person1 == self.person2 or self.people[self.person2] == 0:
+                    self.person2 = randrange(0, self.people_number)
+
+                if self.people[self.person1] != 0:
+                    self.people[self.person1] -= 1
+                    self.people[self.person2] += 1
+        return self.people
+
+    def display_people(self):
+        print(self.people)
+
+def main():
+    b = Beshkan()
+    b.display_people()
+    print(b.beshkan())
+
+
+if __name__ == '__main__':
+    main()
