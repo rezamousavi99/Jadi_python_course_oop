1600f744e182305a89f3e6f864fdb59313d2c90d
rezamousavi <rezamoosavi461@gmail.com>
2023-07-22 17:09:24 +0330

    add pincode.py

---
+++ concept/5. pin code/pincode.py
@@ -0,0 +1,13 @@
+for i in range(0, 100000):
+    ramz = str(i).zfill(5)
+
+    condition1 = int(ramz[2]) + int(ramz[4]) == 14
+    condition2 = int(ramz[0]) == (2 * int(ramz[1])) - 1
+    condition3 = int(ramz[3]) == int(ramz[1]) + 1
+    condition4 = int(ramz[1]) + int(ramz[2]) == 10
+
+    tuple_numbers = tuple(map(int, tuple(ramz)))
+    condition5 = sum(tuple_numbers) == 30
+
+    if condition1 and condition2 and condition3 and condition4 and condition5:
+        print(i)
